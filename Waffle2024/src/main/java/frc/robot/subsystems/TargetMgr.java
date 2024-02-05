760649d018bb09d12a0f6d961c821e35324f5135
dwsindorf <dean.sindorf@gmail.com>
2024-02-05 09:59:17 -0700

    fixed minor build error

--- Waffle2024/src/main/java/frc/robot/subsystems/TargetMgr.java
+++ Waffle2024/src/main/java/frc/robot/subsystems/TargetMgr.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import frc.robot.objects.AprilTag;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.geometry.Translation3d;
