3039c6004075c75d42fda0e7bbdc4f141947bf11
Alpiner <alpinerobotics@gmail.com>
2024-02-09 17:29:15 -0700

    slowing slew rate, adding cubic to x and y input

--- CompBot2024/src/main/java/frc/robot/commands/DriveWithGamepad.java
+++ CompBot2024/src/main/java/frc/robot/commands/DriveWithGamepad.java
@@ -20,9 +20,9 @@ public class DriveWithGamepad extends Command {
   private final Drivetrain m_drive;
 
   // Slew rate limiters to make joystick inputs more gentle; 1/3 sec from 0 to 1.
-  private final SlewRateLimiter m_xspeedLimiter = new SlewRateLimiter(3);
-  private final SlewRateLimiter m_yspeedLimiter = new SlewRateLimiter(3);
-  private final SlewRateLimiter m_rotLimiter = new SlewRateLimiter(3);
+  private final SlewRateLimiter m_xspeedLimiter = new SlewRateLimiter(2);
+  private final SlewRateLimiter m_yspeedLimiter = new SlewRateLimiter(2);
+  private final SlewRateLimiter m_rotLimiter = new SlewRateLimiter(2);
 
  
   /**
@@ -62,13 +62,13 @@ public class DriveWithGamepad extends Command {
 
   private void driveWithJoystick(boolean fieldRelative) {
     // Get the x speed. We are inverting this because Xbox controllers return
-    // negative values when we push forward.
-    final var xSpeed =-m_xspeedLimiter.calculate(MathUtil.applyDeadband(m_controller.getLeftY(), 0.2)) * Drivetrain.kMaxVelocity;
+    // negative values when we push forward. 
+    final var xSpeed =-m_xspeedLimiter.calculate(Math.pow(MathUtil.applyDeadband(m_controller.getLeftY(), 0.2), 3)) * Drivetrain.kMaxVelocity;
 
     // Get the y speed or sideways/strafe speed. We are inverting this because
     // we want a positive value when we pull to the left. Xbox controllers
     // return positive values when you pull to the right by default.
-    final var ySpeed = -m_yspeedLimiter.calculate(MathUtil.applyDeadband(m_controller.getLeftX(), 0.2)) * Drivetrain.kMaxVelocity;
+    final var ySpeed = -m_yspeedLimiter.calculate(Math.pow(MathUtil.applyDeadband(m_controller.getLeftX(), 0.2), 3)) * Drivetrain.kMaxVelocity;
 
     // Get the rate of angular rotation. We are inverting this because we want a
     // positive value when we pull to the left (remember, CCW is positive in
